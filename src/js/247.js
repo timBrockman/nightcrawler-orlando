6bf964f14af0eb6ac9d5e33797a3429fc46b21db
Tim Brockman <tim@timbrockman.com>
2016-07-22 22:01:34 -0400

    ideation complete, base css, init stuff

---
+++ src/js/247.js
@@ -0,0 +1,7 @@
+spots = [
+  {name: "Wawa", lat: 28.552518, lng: -81.396461, phone:"(407) 835-8985", web:"wawa.com", addy: "1200 W Colonial Dr, Orlando, FL 32805"},
+  {name: "7-Eleven", lat: 28.553298, lng: -81.388185, phone:"", web:"", addy: "605 West Colonial Drive, Orlando, FL 32804"},
+  {name: "IHOP", lat: 28.553471, lng: -81.368959, phone:"(407) 896-8313", web: "restaurants.ihop.com", addy: "647 E Colonial Dr, Orlando, FL 32803"},
+  {name: "Steak-n-Shake", lat: 28.552888, lng: -81.347269, phone: "(407) 896-0827", web: "steaknshake.com", addy: "2820 E Colonial Dr, Orlando, FL 32803"},
+  {name: "McDonald's", lat: 28.503997, lng: -81.396276, phone: "(407) 839-3840", web: "mcdonalds.com", addy: "3839 Orange Blossom Trail, Orlando, FL 32839"}
+]
