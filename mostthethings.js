ca78eee1c36ab414fd7756867f0157b7c1d7653e
Tim Brockman <tim@timbrockman.com>
2016-08-07 23:23:22 -0400

    filter applied to markers build

--- mostthethings.js
+++ mostthethings.js
@@ -554,32 +554,34 @@ function Call(data){
   // geocode street address
   //geocodeLocation(processLocation(rawLocation));
   //function geocodeLocation(_location){
-    geocoder.geocode(processLocation(rawLocation),function(results,status){
-      if (status === google.maps.GeocoderStatus.OK) {
-        _self.lat = results[0].geometry.location.lat();
-        _self.lng = results[0].geometry.location.lng();
-        _self.address = results[0].formatted_address;
-        _self.pid =  results[0].place_id;
-        _self.pos    = new google.maps.LatLng(_self.lat, _self.lng);
-        _self.marker = new google.maps.Marker({
-          map: map,
-          position: _self.pos,
-          title: _self.description,
-          animation: google.maps.Animation.DROP,
-          icon: markerIcons[_self.randIcon],
-          id: _self.mid
-        });
-        _self.marker.addListener('mouseover', function() {
-          this.setIcon(markerIcons[4]);
-        });
-        _self.marker.addListener('mouseout', function() {
-          this.setIcon(markerIcons[_self.randIcon]);
-        });
-      }else{
-        console.log('There seems to be a problem: ' + status);
-      }
-      //console.log(_self.mid);
-    });
+  geocoder.geocode(processLocation(rawLocation),function(results,status){
+    if (status === google.maps.GeocoderStatus.OK) {
+      _self.lat = results[0].geometry.location.lat();
+      _self.lng = results[0].geometry.location.lng();
+      _self.address = results[0].formatted_address;
+      _self.pid =  results[0].place_id;
+      _self.pos    = new google.maps.LatLng(_self.lat, _self.lng);
+      _self.marker = new google.maps.Marker({
+        map: map,
+        position: _self.pos,
+        title: _self.description,
+        animation: google.maps.Animation.DROP,
+        icon: markerIcons[_self.randIcon],
+        id: _self.mid
+      });
+      _self.marker.addListener('mouseover', function() {
+        this.setIcon(markerIcons[4]);
+      });
+      _self.marker.addListener('mouseout', function() {
+        this.setIcon(markerIcons[_self.randIcon]);
+      });
+
+    }else{
+      console.log('There seems to be a problem: ' + status);
+    }
+    //console.log(_self.mid);
+  });
+
   //}
 
 }
@@ -642,11 +644,13 @@ function SpotsVM(){
     )
   );
 }
+
 function CallsVM(){
   var self = this;
   var itemCount = 0;
 
   self.onlyAccidents = ko.observable(false);
+
   self.vmCalls = ko.observableArray(
     $.map(mCalls, function(item){
       if(itemCount<10){
@@ -654,16 +658,24 @@ function CallsVM(){
         item.randIcon = Math.floor(Math.random() * 4);
         var currentCall = new Call(item);
         // event listeners added to models
-        
+
         currentCall.mouseoutMarker = function(){
           currentCall.marker.setIcon(markerIcons[currentCall.randIcon]);
         };
         currentCall.mouseoverMarker = function(){
           currentCall.marker.setIcon(markerIcons[4]);
         };
+        currentCall.hideMarker = function(){
+          currentCall.marker.setVisible(currentCall.filterAccident());
+        };
+
         return currentCall;
       }
     })
   );
-
+  self.hideGenCalls = function(){
+    self.vmCalls().forEach(function(item){
+      item.hideMarker();
+    });
+  };
 }
