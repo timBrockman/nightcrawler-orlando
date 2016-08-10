e8c94ca13f0bea585d15dbebb8effb0c031289ae
Tim Brockman <tim@timbrockman.com>
2016-08-10 13:20:32 -0400

    moved applybindings back due to cdn race

--- mostthethings.js
+++ mostthethings.js
@@ -131,14 +131,14 @@ function initMap() {
   //mapInited = true;
 /*
 apply bindings moved to Map Init
-*/
+(moved to window.onload due to async ko & jquery from cdn often being slower than maps)
   ko.applyBindings(SpotsVM, document.getElementById('header'));
   makeRequest("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fwww1.cityoforlando.net%2Fopd%2Factivecalls%2Factivecad.xml'&format=json&diagnostics=true", cbCalls);
   function cbCalls(data){
     mCalls = data;
     ko.applyBindings(CallsVM, document.getElementById('callBox'));
   };
-
+*/
 }
 /*
 models.js
@@ -275,8 +275,22 @@ views.js
 contains the onload stuff
 triggering functions and callbacks that applyBindings
 */
+var applied = false; //incase callback triggered twice or something
+function cbCalls(data){
+  mCalls = data;
+  if(!applied){
+    ko.applyBindings(CallsVM, document.getElementById('callBox'));
+    applied = true;
+  }
+};
+// async defered still blocks onload event ensuring nonrace
+window.onload = function(){
 
   //applies bindings in callback after other crap loads
+    ko.applyBindings(SpotsVM, document.getElementById('header'));
+    makeRequest("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fwww1.cityoforlando.net%2Fopd%2Factivecalls%2Factivecad.xml'&format=json&diagnostics=true", cbCalls);
+  }, 1000);
+};
 /*
 vms.js
 knockout view model class constructors
